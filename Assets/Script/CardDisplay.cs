766c47a5b720e2d4c3430beae4d026780312c850
Jery <jery.rf03@gmail.com>
2024-03-07 12:17:39 -0500

    ErrorSolve

--- Assets/Script/CardDisplay.cs
+++ Assets/Script/CardDisplay.cs
@@ -11,9 +11,14 @@ public class CardDisplay : MonoBehaviour
     public Image artWork;
     public Image portrait;
 
-
+    //public Transform parentObject;
     void Start()
     {
+        
+        //textPower.transform.SetParent(parentObject, false);
+        //artWork.transform.SetParent(parentObject, false);
+        //portrait.transform.SetParent(parentObject, false);
+
         //card.child.transform.SetParent();
         textPower.text = card.power.ToString();
         artWork.sprite = card.artWork;
---
+++ Assets/Script/Panels.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panels : MonoBehaviour
+{
+    public List<Card> cards;
+    public int maxItems;
+    public int itemsCounter;
+}
+
