2c98885d8c92562f8c2afb312e31a3459727e7be
Jery <jery.rf03@gmail.com>
2024-03-23 12:48:26 -0400

    CardPanel

--- Assets/Script/Drop.cs
+++ Assets/Script/Drop.cs
@@ -19,11 +19,14 @@ public class Drop : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerE
     }
     public void OnPointerEnter(PointerEventData evenData)            // Se ejecuta cuando el puntero entra en el área del objeto
     {
-        GameObject.Find("Panel_Card").transform.GetChild(2).GetComponent<Text>().text = "2";
+
+        //if (evenData.pointerDrag.GetComponent<Drag>() != null)
+        //    GameObject.Find("Panel_Card").transform.GetChild(2).GetComponent<Text>().text = evenData.pointerDrag.GetComponent<CardDisplay>().textPower.text;
     }
     public void OnPointerExit(PointerEventData evenData)            //  Se ejecuta cuando el puntero sale del área del objeto.
     {
 
+        //GameObject.Find("Panel_Card").transform.GetChild(2).GetComponent<Text>().text = "";
     }
     public void OnDrop(PointerEventData evenData)                   // Se ejecuta cuando un objeto es soltado sobre el objeto asociado a este script.
     {
---
+++ Assets/Script/DropCard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropCard : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData evenData)            // Se ejecuta cuando el puntero entra en el área del objeto
+    {
+        if (this != null)
+        {
+            GameObject.Find("Panel_Card").transform.GetChild(2).GetComponent<Text>().text = this.GetComponent<CardDisplay>().textPower.text;
+            GameObject.Find("Panel_Card").transform.GetChild(0).GetComponent<Image>().sprite = this.GetComponent<CardDisplay>().card.artWork;
+            GameObject.Find("Panel_Card").transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = this.GetComponent<CardDisplay>().card.portrait;
+        }
+        
+    }
+    public void OnPointerExit(PointerEventData evenData)            //  Se ejecuta cuando el puntero sale del área del objeto.
+    {
+        GameObject.Find("Panel_Card").transform.GetChild(2).GetComponent<Text>().text = "";
+        GameObject.Find("Panel_Card").transform.GetChild(0).GetComponent<Image>().sprite = null;
+        GameObject.Find("Panel_Card").transform.GetChild(0).transform.GetChild(0).GetComponent<Image>().sprite = null;
+    }
+    public void OnDrop(PointerEventData evenData)                   // Se ejecuta cuando un objeto es soltado sobre el objeto asociado a este script.
+    {
+
+    }
+}
